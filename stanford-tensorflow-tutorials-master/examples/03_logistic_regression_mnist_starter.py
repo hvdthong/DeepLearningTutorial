7dc61e4ee46a1fb391543e03207094783c49ba54
Thong Hoang <hvdthong@Thongs-MacBook-Pro.local>
2017-08-15 16:37:41 +0800

    adding logistic regression

--- stanford-tensorflow-tutorials-master/examples/03_logistic_regression_mnist_starter.py
+++ stanford-tensorflow-tutorials-master/examples/03_logistic_regression_mnist_starter.py
@@ -9,7 +9,6 @@ import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-import numpy as np
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import time
@@ -17,42 +16,45 @@ import time
 # Define paramaters for the model
 learning_rate = 0.01
 batch_size = 128
-n_epochs = 10
+n_epochs = 25
 
 # Step 1: Read in data
 # using TF Learn's built in function to load MNIST data to the folder data/mnist
-mnist = input_data.read_data_sets('/data/mnist', one_hot=True)
+mnist = input_data.read_data_sets('./data/MNIST', one_hot=True)
 
 # Step 2: create placeholders for features and labels
 # each image in the MNIST data is of shape 28*28 = 784
 # therefore, each image is represented with a 1x784 tensor
 # there are 10 classes for each image, corresponding to digits 0 - 9. 
 # Features are of the type float, and labels are of the type int
-
+X = tf.placeholder(tf.float32, [None, 784], name='X_placeholder')
+Y = tf.placeholder(tf.int32, [None, 10], name='Y_placeholder')
 
 # Step 3: create weights and bias
 # weights and biases are initialized to 0
 # shape of w depends on the dimension of X and Y so that Y = X * w + b
 # shape of b depends on Y
-
+w = tf.Variable(tf.random_normal(shape=[784, 10], stddev=0.01), name="weights")
+b = tf.Variable(tf.zeros([1, 10]), name="bias")
 
 # Step 4: build model
 # the model that returns the logits.
 # this logits will be later passed through softmax layer
 # to get the probability distribution of possible label of the image
 # DO NOT DO SOFTMAX HERE
-
+logits = tf.matmul(X, w) + b
 
 # Step 5: define loss function
 # use cross entropy loss of the real labels with the softmax of logits
 # use the method:
 # tf.nn.softmax_cross_entropy_with_logits(logits, Y)
 # then use tf.reduce_mean to get the mean loss of the batch
-
+entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y)
+loss = tf.reduce_mean(entropy, name="loss")
 
 # Step 6: define training op
 # using gradient descent to minimize loss
-
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 
 with tf.Session() as sess:
     start_time = time.time()
@@ -64,8 +66,7 @@ with tf.Session() as sess:
         for _ in range(n_batches):
             X_batch, Y_batch = mnist.train.next_batch(batch_size)
             # TO-DO: run optimizer + fetch loss_batch
-            #
-            #
+            _, loss_batch = sess.run([optimizer, loss], feed_dict={X: X_batch, Y: Y_batch})
             total_loss += loss_batch
         print('Average loss epoch {0}: {1}'.format(i, total_loss / n_batches))
 
@@ -84,6 +85,6 @@ with tf.Session() as sess:
     for i in range(n_batches):
         X_batch, Y_batch = mnist.test.next_batch(batch_size)
         accuracy_batch = sess.run([accuracy], feed_dict={X: X_batch, Y: Y_batch})
-        total_correct_preds += accuracy_batch
+        total_correct_preds += accuracy_batch[0]
 
     print('Accuracy {0}'.format(total_correct_preds / mnist.test.num_examples))
