989ca26a657336ff8089577bb61decfa2704f655
Thong Hoang <hvdthong@Thongs-MacBook-Pro.local>
2017-10-13 16:44:52 +0800

    updating reader

--- stanford-tensorflow-tutorials-master/examples/05_csv_reader.py
+++ stanford-tensorflow-tutorials-master/examples/05_csv_reader.py
@@ -10,9 +10,11 @@ Prepared for the class CS 20SI: "TensorFlow for Deep Learning Research"
 cs20si.stanford.edu
 """
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import sys
+
 sys.path.append('..')
 
 import tensorflow as tf
@@ -21,12 +23,13 @@ DATA_PATH = 'data/heart.csv'
 BATCH_SIZE = 2
 N_FEATURES = 9
 
+
 def batch_generator(filenames):
     """ filenames is the list of files you want to read from. 
     In this case, it contains only heart.csv
     """
     filename_queue = tf.train.string_input_producer(filenames)
-    reader = tf.TextLineReader(skip_header_lines=1) # skip the first line in the file
+    reader = tf.TextLineReader(skip_header_lines=1)  # skip the first line in the file
     _, value = reader.read(filename_queue)
 
     # record_defaults are the default values in case some of our columns are empty
@@ -41,10 +44,11 @@ def batch_generator(filenames):
     record_defaults.append([1])
 
     # read in the 10 rows of data
-    content = tf.decode_csv(value, record_defaults=record_defaults) 
+    content = tf.decode_csv(value, record_defaults=record_defaults)
 
     # convert the 5th column (present/absent) to the binary value 0 and 1
-    content[4] = tf.cond(tf.equal(content[4], tf.constant('Present')), lambda: tf.constant(1.0), lambda: tf.constant(0.0))
+    content[4] = tf.cond(tf.equal(content[4], tf.constant('Present')), lambda: tf.constant(1.0),
+                         lambda: tf.constant(0.0))
 
     # pack all 9 features into a tensor
     features = tf.stack(content[:N_FEATURES])
@@ -61,24 +65,27 @@ def batch_generator(filenames):
     capacity = 20 * BATCH_SIZE
 
     # shuffle the data to generate BATCH_SIZE sample pairs
-    data_batch, label_batch = tf.train.shuffle_batch([features, label], batch_size=BATCH_SIZE, 
-                                        capacity=capacity, min_after_dequeue=min_after_dequeue)
+    data_batch, label_batch = tf.train.shuffle_batch([features, label], batch_size=BATCH_SIZE,
+                                                     capacity=capacity, min_after_dequeue=min_after_dequeue)
 
     return data_batch, label_batch
+
 
 def generate_batches(data_batch, label_batch):
     with tf.Session() as sess:
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(coord=coord)
-        for _ in range(10): # generate 10 batches
+        for _ in range(10):  # generate 10 batches
             features, labels = sess.run([data_batch, label_batch])
             print(features)
         coord.request_stop()
         coord.join(threads)
 
+
 def main():
     data_batch, label_batch = batch_generator([DATA_PATH])
     generate_batches(data_batch, label_batch)
+
 
 if __name__ == '__main__':
     main()
